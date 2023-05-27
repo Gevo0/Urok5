3345c9ecf5be392bb29417b74b4e4da6540c4fd2
Gevorg <bezhanyangevorg@gmail.com>
2023-05-27 18:54:53 +0400

    5 дз

---
+++ DZ-34/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными 
+числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2*/
+
+
+
+Console.WriteLine("Введите размер массива ");
+
+int size = int.Parse(Console.ReadLine());
+
+int[] num = new int[size];
+
+Random rand = new Random();
+
+for (int i = 0; i < num.Length; i++)
+{
+    num[i] = rand.Next(100, 1000);
+    Console.WriteLine(num[i]);
+}
+
+int Count = 0;
+foreach (int number in num)
+{
+if (number % 2 == 0)
+
+{
+ Count ++; 
+}
+    
+}
+Console.WriteLine("Количество четных чисел в массиве: " + Count);
